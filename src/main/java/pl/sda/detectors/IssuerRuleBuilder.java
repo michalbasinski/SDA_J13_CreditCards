05fe3d816a379414672212c549c51edd442af188
Michał Basiński <michal.basinski88@gmail.com>
2019-02-23 15:32:17 +0100

    Issuer rule file builder

---
+++ src/main/java/pl/sda/detectors/IssuerRuleBuilder.java
@@ -0,0 +1,15 @@
+package pl.sda.detectors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IssuerRuleBuilder {
+
+    public List<IssuerRule> buildIssuerRules() {
+        List<IssuerRule> issuerRules = new ArrayList<>();
+
+        //TODO: zbudować listę issuer rules korzystając z plku issuers.csv
+
+        return issuerRules;
+    }
+}
