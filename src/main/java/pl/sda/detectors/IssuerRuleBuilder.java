5db200e67489c260327f4fc9dff324db54f77dab
Michał Basiński <michal.basinski88@gmail.com>
2019-02-24 09:33:38 +0100

    try-with-resources

--- src/main/java/pl/sda/detectors/IssuerRuleBuilder.java
+++ src/main/java/pl/sda/detectors/IssuerRuleBuilder.java
@@ -1,5 +1,9 @@
 package pl.sda.detectors;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +12,21 @@ public class IssuerRuleBuilder {
     public List<IssuerRule> buildIssuerRules() {
         List<IssuerRule> issuerRules = new ArrayList<>();
 
-        //TODO: zbudować listę issuer rules korzystając z plku issuers.csv
+        String resourcePath = getClass()
+                .getClassLoader()
+                .getResource("issuer.csv")
+                .getPath();
 
+        //konstrukcja try-with-resources - bufferedReader/fileReader zostają zamknięte przez JVM w momencie błędu
+        //więcej informacji - https://www.samouczekprogramisty.pl/konstrukcja-try-with-resources-w-jezyku-java/
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(resourcePath))) {
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return issuerRules;
     }
 }
